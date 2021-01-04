d76477cdd88768f4efca59003bcfca3a1189f1c3
tshuhei <tsushuhei@gmail.com>
2021-01-04 12:08:33 +0900

    kadai completed

--- front-page.php
+++ front-page.php
@@ -27,4 +27,58 @@
 	</div>
 </div>
 
+<div class="kadai">
+	<div class="kadai-title-wrapper">
+		<p class="kadai-title">こんな課題を抱えていませんか？</p>
+	</div>
+	<div class="kadai-1">
+		<div class="kadai1-left">
+			<img class="kadai-image" src="<?php echo get_theme_file_uri(); ?>/images/kadai1_clipped.png">
+		</div>
+		<div class="kadai1-right">
+			<p class="kadai-lr-title"><span class="first-character">ホ</span>ームページ制作が高額...</p>
+			<p class="kadai-lr-description">
+				吾輩わがはいは猫である。名前はまだ無い。どこで生れたかとんと見当
+				けんとうがつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いて
+				いた事だけは記憶している。吾輩はここで始めて人間というものを見た。
+				しかもあとで聞くとそれは書生という人間中で一番獰悪どうあくな種族で
+				あったそうだ。この書生というのは時々我々を捕つかまえて煮にて食うと
+				いう話である。しかしその当時は何という考
+			</p>
+		</div>
+	</div>
+	<div class="kadai-2">
+		<div class="kadai2-left">
+			<p class="kadai-lr-title"><span class="first-character">制</span>作にかなりの時間がかかる...</p>
+			<p class="kadai-lr-description">
+			吾輩わがはいは猫である。名前はまだ無い。どこで生れたかとんと見当
+				けんとうがつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いて
+				いた事だけは記憶している。吾輩はここで始めて人間というものを見た。
+				しかもあとで聞くとそれは書生という人間中で一番獰悪どうあくな種族で
+				あったそうだ。この書生というのは時々我々を捕つかまえて煮にて食うと
+				いう話である。しかしその当時は何という考
+			</p>
+		</div>
+		<div class="kadai2-right">
+			<img class="kadai-image" src="<?php echo get_theme_file_uri(); ?>/images/kadai2_clipped.png">
+		</div>
+	</div>
+	<div class="kadai-3">
+		<div class="kadai3-left">
+			<img class="kadai-image" src="<?php echo get_theme_file_uri(); ?>/images/kadai3_clipped.png">
+		</div>
+		<div class="kadai3-right">
+			<p class="kadai-lr-title"><span class="first-character">イ</span>メージ通りのデザインにならない...</p>
+			<p class="kadai-lr-description">
+			吾輩わがはいは猫である。名前はまだ無い。どこで生れたかとんと見当
+				けんとうがつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いて
+				いた事だけは記憶している。吾輩はここで始めて人間というものを見た。
+				しかもあとで聞くとそれは書生という人間中で一番獰悪どうあくな種族で
+				あったそうだ。この書生というのは時々我々を捕つかまえて煮にて食うと
+				いう話である。しかしその当時は何という考
+			</p>
+		</div>
+	</div>
+</div>
+
 <?php get_footer();?>
