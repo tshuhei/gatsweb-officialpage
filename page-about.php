5c84691b6b6210ac1730c67aa66b44caae07eb9a
tshuhei <tsushuhei@gmail.com>
2021-01-06 14:12:11 +0900

    about page completed

--- page-about.php
+++ page-about.php
@@ -1,5 +1,45 @@
 <?php get_header(); ?>
 
-	<h1> This is the about page </h1>
+<div class="about">
+	<div class="about-top">
+		<img class="about-top-arrow" src="<?php echo get_theme_file_uri(); ?>/images/left-arrow.png">
+		<p class="about-top-str">About</p>
+		<img class="about-top-arrow" src="<?php echo get_theme_file_uri(); ?>/images/right_arrow.png">
+	</div>
+	<div class="about-top-title-wrapper">
+		<p class="about-top-title">Mission</p>
+	</div>
+	<p class="about-top-description">
+		吾輩わがはいは猫である。名前はまだ無い。どこで生れたかとんと見当けんとうがつかぬ。何でも薄暗いじめじめした
+		所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くと
+		それは書生という人間中で一番獰悪どうあくな種族であったそうだ。この書生というのは時々我々を捕つかまえて煮に
+		て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌てのひ
+		らに載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生
+		の顔を見たのがいわゆる人間というものの見始みはじめであろう。この時妙なものだと思った感じが今でも残っている。
+		第一毛を
+	</p>
+</div>
+
+<div class="greeting">
+	<div class="greeting-title-wrapper">
+		<p class="greeting-title">代表からのご挨拶</p>
+	</div>
+	<div class="greeting-contents">
+		<p class="greeting-discription">
+			吾輩わがはいは猫である。名前はまだ無い。どこで生れたかとん
+			と見当けんとうがつかぬ。何でも薄暗いじめじめした所でニャー
+			ニャー泣いていた事だけは記憶している。吾輩はここで始めて人
+			間というものを見た。しかもあとで聞くとそれは書生という人間
+			中で一番獰悪どうあくな種族であったそうだ。この書生というの
+			は時々我々を捕つかまえて煮にて食うという話である。しかしそ
+			の当時は何という考もなかったから別段恐しいとも思わなかった
+			。ただ彼の掌てのひらに載せられてスーと持ち上げられた時何だ
+			かフワフワした感じがあったばかりである。掌の上で少し落ちつ
+			いて書生の顔を見たのがいわゆる人間というものの見始みはじめ
+			であろう。この時妙なものだと思った感じが今でも残っている。
+			第一毛を</p>
+		<img class="greeting-president" src="<?php echo get_theme_file_uri(); ?>/images/president_clipped.png">
+	</div>
+</div>
 
 <?php get_footer();?>
