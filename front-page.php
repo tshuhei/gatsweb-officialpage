d92b184f0c783bd129f3032a996613af211d3c1b
tshuhei <tsushuhei@gmail.com>
2021-04-23 19:02:55 +0900

    updated Mission and Vision

--- front-page.php
+++ front-page.php
@@ -5,9 +5,11 @@
 		<div class="hero">
 			<p class="hero-title onload">GatsWeb</p>
 			<p class="hero-subtitle onload delay1">Websites for all</p>
-			<a href="https://gatsweb.net/contact"><p class="hero-cta onload delay3">制作依頼・問合せはこちら</p></a>
+			<a href="https://gatsweb.net/contact">
+				<p class="hero-cta onload delay3">制作依頼・問合せはこちら</p>
+			</a>
 			<p class="hero-description onload delay2">
-			最小限のコストで最大限のアピールを。GatsWebはあなたのWebサイトデビューを応援します。
+				最小限のコストで最大限のアピールを。GatsWebはあなたのWebサイトデビューを応援します。
 			</p>
 		</div>
 	</div>
@@ -30,7 +32,7 @@
 			<div class="kadai1-right">
 				<p class="kadai-lr-title"><span class="first-character">ホ</span>ームページ制作が高額...</p>
 				<p class="kadai-lr-description">
-				Webサイト制作にかかる相場をご存知ですか？おおよその相場が20万から30万円。一般的な企業が用いるようなものであれば100万円を超えることもしばしば。安くても3～10万円かかると言われています。
+					Webサイト制作にかかる相場をご存知ですか？おおよその相場が20万から30万円。一般的な企業が用いるようなものであれば100万円を超えることもしばしば。安くても3～10万円かかると言われています。
 				</p>
 			</div>
 		</div>
@@ -40,7 +42,7 @@
 			<div class="kadai2-left">
 				<p class="kadai-lr-title"><span class="first-character">制</span>作にかなりの時間がかかる...</p>
 				<p class="kadai-lr-description">
-				Webサイト制作にかかる期間はおよそ2カ月から3カ月ほどが平均です。1ページのみのサイトでも2カ月弱かかることもしばしば。
+					Webサイト制作にかかる期間はおよそ2カ月から3カ月ほどが平均です。1ページのみのサイトでも2カ月弱かかることもしばしば。
 				</p>
 			</div>
 			<div class="kadai2-right">
@@ -56,7 +58,7 @@
 			<div class="kadai3-right">
 				<p class="kadai-lr-title"><span class="first-character">イ</span>メージ通りのデザインにならない...</p>
 				<p class="kadai-lr-description">
-				安価なWeb製作会社に依頼した場合、テンプレートを用いた制作になる場合がほとんどです。気に入ったレイアウトにならなかったり、同業他社とページを差別化するのが難しくなることも。
+					安価なWeb製作会社に依頼した場合、テンプレートを用いた制作になる場合がほとんどです。気に入ったレイアウトにならなかったり、同業他社とページを差別化するのが難しくなることも。
 				</p>
 			</div>
 		</div>
@@ -72,7 +74,7 @@
 			<img class="reason-item-image" src="<?php echo get_theme_file_uri(); ?>/images/reason1_clipped2.png">
 			<p class="reason-item-title">業界最安値！</p>
 			<p class="reason-item-description-first">
-			小規模組織のため、運営費や無駄な仲介手数料などはほぼ無し！納得の業界最安値を提供いたします。
+				小規模組織のため、運営費や無駄な仲介手数料などはほぼ無し！納得の業界最安値を提供いたします。
 			</p>
 			<p class="reason-item-description-attention">※要求されるクオリティによってお値段は上下することがあります。</p>
 		</div>
@@ -80,7 +82,7 @@
 			<img class="reason-item-image" src="<?php echo get_theme_file_uri(); ?>/images/reason2_clipped.png">
 			<p class="reason-item-title">素早い納品！</p>
 			<p class="reason-item-description-first">
-			スピードコースなら、最短2週間ほどでの納品が可能！
+				スピードコースなら、最短2週間ほどでの納品が可能！
 			</p>
 			<p class="reason-item-description-attention">※相談内容によっては長引く場合がございます。</p>
 		</div>
@@ -88,7 +90,7 @@
 			<img class="reason-item-image" src="<?php echo get_theme_file_uri(); ?>/images/reason3_clipped.png">
 			<p class="reason-item-title">徹底したヒアリング！</p>
 			<p class="reason-item-description">
-			納得のいくデザインで製作できるまで、担当エンジニアがご相談させていただきます。
+				納得のいくデザインで製作できるまで、担当エンジニアがご相談させていただきます。
 			</p>
 		</div>
 	</div>
@@ -104,7 +106,7 @@
 			<div class="process-item-text-wrapper">
 				<p class="process-item-title"><span class="process-first-character">1</span>ヒアリング</p>
 				<p class="process-item-description">
-				お客様のご要望を細かくお聞きし、用途・ターゲット・ご予算に合わせたぴったりのプランをご提案いたします。
+					お客様のご要望を細かくお聞きし、用途・ターゲット・ご予算に合わせたぴったりのプランをご提案いたします。
 				</p>
 			</div>
 		</div>
@@ -114,10 +116,10 @@
 		</div>
 		<div id="process-item-2" class="process-item process-up delay1">
 			<img class="process-item-image" src="<?php echo get_theme_file_uri(); ?>/images/process2_clipped.png">
-			<div class="process-item-text-wrapper">	
+			<div class="process-item-text-wrapper">
 				<p class="process-item-title"><span class="process-first-character">2</span>デザイン提案</p>
 				<p class="process-item-description">
-				お客様のご要望に合わせたデザインを提案いたします。気に入らなければ何度でも修正することが可能です。
+					お客様のご要望に合わせたデザインを提案いたします。気に入らなければ何度でも修正することが可能です。
 				</p>
 			</div>
 		</div>
@@ -130,7 +132,7 @@
 			<div class="process-item-text-wrapper">
 				<p class="process-item-title"><span class="process-first-character">3</span>制作</p>
 				<p class="process-item-description">
-				実際に制作を行います。なるべくスピーディーに、かつ高クオリティでの制作をお約束いたします。
+					実際に制作を行います。なるべくスピーディーに、かつ高クオリティでの制作をお約束いたします。
 				</p>
 			</div>
 		</div>
@@ -143,7 +145,7 @@
 			<div class="process-item-text-wrapper">
 				<p class="process-item-title"><span class="process-first-character">4</span>運用</p>
 				<p class="process-item-description">
-				制作したWebサービスをお客様にお試しいただきます。機能拡張・修正だけでなく運用方法やその他質問がございましたら、お気軽にお問い合わせください。
+					制作したWebサービスをお客様にお試しいただきます。機能拡張・修正だけでなく運用方法やその他質問がございましたら、お気軽にお問い合わせください。
 				</p>
 			</div>
 		</div>
@@ -156,18 +158,10 @@
 			<p>Vision</p>
 		</div>
 		<div class="vision-description up">
-			<p>Webサイトのメリットとして、SNS等と異なり流行り廃りが存在しないことが挙げられます。<br>
-				Twitter,instagramをお使いですか?SNSは非常に便利なツールですが、果たして10年後も
-				変わらず使われているでしょうか？
-				せっかく集めたフォロワーが水の泡…なんてことにもなりかねません。
-				Webサイトなら、そんな心配はありません。Googleなどの検索エンジンがある限り、
-				集客ツールとして機能し続けます。
-			</p>
 			<p>
-			また、Webサイトを持つことでお客様からの信用を得られるというメリットもあります。<br>
-			「初めてのお店だから不安」「この人に仕事を任せても平気かな？」そんな不安から
-			お客様はあなたを検索します。そんな時、Webサイトはアピールができると同時に
-			あなたの信用度を非常に高めます。
+				もっとたくさんの人に自分や自分のサービスを知ってほしい。「自分のWebサイト」を持ってみたい。
+				そんな方が気軽に運用を始められるWebサイト制作会社に。
+				"オンライン上の名刺"を誰もが持っている社会の実現に最大限貢献します。
 			</p>
 		</div>
 	</div>
@@ -184,9 +178,11 @@
 				<!-- Tell：123-1234-1234<br> -->
 				Email：contact@gatsweb.com
 			</p>
-			<a href="https://gatsweb.net/contact"><p class="contact-cta">詳しくはこちらから</p></a>
+			<a href="https://gatsweb.net/contact">
+				<p class="contact-cta">詳しくはこちらから</p>
+			</a>
 		</div>
 	</div>
 </div>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
